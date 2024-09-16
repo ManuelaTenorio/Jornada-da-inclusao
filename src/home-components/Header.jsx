78662419309a8dc6442c814c933fc3ec98bdb993
Marcos Vinícius <marcos@fatec>
2024-09-16 10:57:51 -0300

    Incluindo /src/ e /public/

---
+++ src/home-components/Header.jsx
@@ -0,0 +1,15 @@
+import './header-footer.css';
+
+function Header() {
+    return (
+        <div class="head">
+            <li><img src="/Pi LOGO.png" alt="Logo da página Integra Kids, ilustrando um garoto encima de um foguete" /></li>
+            <li><input type="button" value="Página Principal" /></li>
+            <li><input type="button" value="Lista de Jogos" /></li>
+            <li><input type="button" value="Sobre Nós" /></li>
+            <li><input type="button" value="Login/Cadastro" /></li>
+            </div>
+    )
+}
+
+export default Header
