0a043c6734b798fedf74c1eac66933f78c32b5e4
renatowljacob <renato.jacob@fatec.sp.gov.br>
2024-09-16 04:03:17 -0300

    Randomização dos números

--- JornadaDaInclusao/src/pages/jogo2.tsx
+++ JornadaDaInclusao/src/pages/jogo2.tsx
@@ -1,47 +1,52 @@
-import '../jogo2.css'
+import { DropBox } from '../components/DropBox'
+import { NumberBox } from '../components/NumberBox'
+import '../assets/jogo2.css'
+
+function Randomize() {
+    function GetRandom(arr: number[]) {
+        let currIndex = arr.length;
+
+        while (currIndex != 0) {
+            let randomIndex = Math.floor(Math.random() * currIndex);
+            currIndex--;
+
+            [arr[currIndex], arr[randomIndex]] = [
+                arr[randomIndex], arr[currIndex]
+            ];
+        }
+    }
+
+    let numbers: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    GetRandom(numbers);
+
+    return numbers;
+}
 
 export default function Jogo2() {
+    const arr: number[] = Randomize();
+
     return (
         <>
             <div className="container">
                 <h1>Jogo dos Números</h1>
-                <p>Arraste os números para cima na sequência correta</p>
+                <p>Arraste os números para cima na sequência correta.</p>
                 <div className="drop-area">
-                    <div className="drop-box"></div>
+                    <DropBox />
                 </div>
                 <div className="number-area">
-                    <div className="number-box">
-                        <span>5</span>
-                    </div>
-                    <div className="number-box">
-                        <span>8</span>
-                    </div>
-                    <div className="number-box">
-                        <span>9</span>
-                    </div>
-                    <div className="number-box">
-                        <span>1</span>
-                    </div>
-                    <div className="number-box">
-                        <span>7</span>
-                    </div>
-                    <div className="number-box">
-                        <span>2</span>
-                    </div>
-                    <div className="number-box">
-                        <span>6</span>
-                    </div>
-                    <div className="number-box">
-                        <span>0</span>
-                    </div>
-                    <div className="number-box">
-                        <span>3</span>
-                    </div>
-                    <div className="number-box">
-                        <span>4</span>
-                    </div>
+                    <NumberBox value={arr[0]} />
+                    <NumberBox value={arr[1]} />
+                    <NumberBox value={arr[2]} />
+                    <NumberBox value={arr[3]} />
+                    <NumberBox value={arr[4]} />
+                    <NumberBox value={arr[5]} />
+                    <NumberBox value={arr[6]} />
+                    <NumberBox value={arr[7]} />
+                    <NumberBox value={arr[8]} />
+                    <NumberBox value={arr[9]} />
                 </div>
-            </div>
+            </div >
         </>
     )
 }
