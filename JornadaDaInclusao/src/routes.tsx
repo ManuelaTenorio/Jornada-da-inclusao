f44716f36a8bd49abb4ce93d90756028e7b4956a
renatowljacob <renato.jacob@fatec.sp.gov.br>
2024-09-21 01:11:28 -0300

    Refactoring: automating number generation

--- JornadaDaInclusao/src/routes.tsx
+++ JornadaDaInclusao/src/routes.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
-import Jogo2 from "./pages/jogo2";
+import JogoNumeros from "./pages/jogoNumeros";
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="jogo2" element={<Jogo2 />} />
+                <Route path="jogoNumeros" element={<JogoNumeros />} />
             </Routes>
         </BrowserRouter>
     )
