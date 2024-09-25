a33a11f42bdbe25957a23a05577f46d6086267f5
pedro-hsb <pedro.bernardo4561@gmail.com>
2024-09-24 21:20:58 -0300

    merge jogoCores

---
+++ JornadaDaInclusao/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import AppRoutes from "./routes";
+
+export default function App() {
+  
+
+  return (
+    <AppRoutes />
+  );
+}
