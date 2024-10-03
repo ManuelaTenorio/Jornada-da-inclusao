fc6b39f2acda1c45142d2c0f6a5e38a715ee6e1e
renatowljacob <renato.jacob@fatec.sp.gov.br>
2024-10-02 21:57:30 -0300

    Fixing letters resizing to container's size

--- src/components/letters/Letters.jsx
+++ src/components/letters/Letters.jsx
@@ -3,39 +3,40 @@ import { CSS } from "@dnd-kit/utilities";
 import './letters.css'
 
 function Letters({ id, value }) {
-    const { attributes, listeners, setNodeRef, transform, transition } = useDraggable({id});
+  const { attributes, listeners, setNodeRef, transform, transition } = useDraggable({ id });
 
-    const staticStyles  = {
-      width: '50px',
-      height: '50px',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontSize: '20px',
-      color: 'white',
-      borderRadius: '8px',
-      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-      willChange: 'transform',
+  const staticStyles = {
+    width: '50px',
+    height: '50px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: '20px',
+    color: 'white',
+    borderRadius: '8px',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+    willChange: 'transform',
   };
-  const dynamicStyles = {
-    transform: CSS.Transform.toString(transform),
-    transition,
-};
-  
 
-    return (
-      <div className='letters'>
-        <div
-            className="letter"
-            ref={setNodeRef}
-            style={{...staticStyles , ...dynamicStyles}}
-            {...attributes}
-            {...listeners}
-        >
-            {String.fromCharCode(64 + value)}
-        </div>
-        </div>
-    );
+  const dynamicStyles = {
+    transform: CSS.Translate.toString(transform),
+    transition,
+  };
+
+
+  return (
+    <div className='letters'>
+      <div
+        className="letter"
+        ref={setNodeRef}
+        style={{ ...staticStyles, ...dynamicStyles }}
+        {...attributes}
+        {...listeners}
+      >
+        {String.fromCharCode(64 + value)}
+      </div>
+    </div>
+  );
 }
 
 export default Letters;
