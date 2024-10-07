9fa5678626233fc345e2aa530e716a0d0e2d4e1a
pedro-hsb <pedro.bernardo4561@gmail.com>
2024-10-07 11:45:13 -0300

    adjustment in login.css

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,20 +4,22 @@ import './login.css'
 function Login() {
     return (
         <>
-            <div id="login-container">
-                <h1></h1>
-                <form id="login-form">
-                    <div className="full-box">
-                        <label for="email">E-mail</label>
-                        <input type="email" id="email" name="email" placeholder="Digite seu e-mail" required />
-                    </div>
-                    <div className="full-box">
-                        <label for="password">Senha</label>
-                        <input type="password" id="password" name="password" placeholder="Digite sua senha" required />
-                    </div>
-                    <input type="submit" value="Entrar" />
-                    <p>Não tem uma conta? <a href="#">Cadastre-se</a></p>
-                </form>
+            <div className="bodyLogin">
+                <div id="login-container">
+                    <h1></h1>
+                    <form id="login-form">
+                        <div className="full-box">
+                            <label for="email">E-mail</label>
+                            <input type="email" id="email" name="email" placeholder="Digite seu e-mail" required />
+                        </div>
+                        <div className="full-box">
+                            <label for="password">Senha</label>
+                            <input type="password" id="password" name="password" placeholder="Digite sua senha" required />
+                        </div>
+                        <input type="submit" value="Entrar" />
+                        <p>Não tem uma conta? <a href="#">Cadastre-se</a></p>
+                    </form>
+                </div>
             </div>
         </>
     )
