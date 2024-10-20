7486272817e61252936832d15e4f73adcd99efbd
ViniMarkos283 <102962949+ViniMarkos283@users.noreply.github.com>
2024-10-20 16:51:52 -0300

    responsive update
    - adjust responsive reolution in homepage components

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,18 @@
 import Header from "../components/home/Header.jsx";
-import Carousel from "../components/home/Carousel.jsx"
-import Cards from "../components/home/Cards.jsx"
+import Carousel from "../components/home/Carousel.jsx";
+import Cards from "../components/home/Cards.jsx";
 import Contact from '../components/home/Contact.jsx';
 import About from '../components/home/About.jsx';
-import Footer from "../components/home/Footer.jsx"
+import Footer from "../components/home/Footer.jsx";
 
 import '../assets/styles/home.css';
 import backgroundImage from '../assets/images/background-home.jpg';
 
 const Home = () => (
   <div className="home" style={{
-    backgroundImage: `url(${backgroundImage})`}}>
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundAttachment: 'fixed', 
+  }}>
     <Header />
     <Carousel />
     <h1>Confira os jogos da nossa plataforma</h1>
@@ -22,5 +24,3 @@ const Home = () => (
 );
 
 export default Home;
-
-
